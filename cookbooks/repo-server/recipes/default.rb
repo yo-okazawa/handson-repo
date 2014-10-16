8303e6a18e3a74d4598ee068b827585730829fbf
Hiroyuki Urasoko <ura.58.58@gmail.com>
2014-10-16 17:16:24 +0900

    Initial:repo server cookbook

---
+++ cookbooks/repo-server/recipes/default.rb
@@ -0,0 +1,13 @@
+#
+# Cookbook Name:: repo-server
+# Recipe:: default
+#
+# Copyright (c) 2014 The Authors, All Rights Reserved.
+rpm_package "nginx" do
+  source "/tmp/nginx-1.7.6-1.el6.ngx.x86_64.rpm"
+end
+
+service 'nginx' do
+  supports :status => true, :restart => true, :reload => true
+  action [:enable, :start]
+end
