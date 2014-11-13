f668b283b48bdc25cc7825bfb028e0fb7b59d53e
Hiroyuki Urasoko <ura.58.58@gmail.com>
2014-11-13 18:45:01 +0900

    install reporting to all servers

--- cookbooks/chef-server/recipes/fe.rb
+++ cookbooks/chef-server/recipes/fe.rb
@@ -15,13 +15,6 @@ execute "chef-server-ctl install opscode-manage --path #{node['chef-server']['in
 
 execute "opscode-manage-ctl reconfigure"
 
-remote_file "#{node['chef-server']['install_path']}/#{node['chef-server']['report']['package']}" do
-  source "http://#{node['chef-server']['repo']['fqdn']}/#{node['chef-server']['report']['package']}"
-  checksum "#{node['chef-server']['report']['checksum']}"
-end
-
-execute "chef-server-ctl install opscode-reporting --path #{node['chef-server']['install_path']}"
-
-execute "opscode-reporting-ctl reconfigure"
+execute "chef-server-ctl reconfigure"
 
 include_recipe "chef-server::post"
--- cookbooks/chef-server/recipes/post.rb
+++ cookbooks/chef-server/recipes/post.rb
@@ -1,19 +1,28 @@
-remote_file "#{node['chef-server']['install_path']}/#{node['chef-server']['push']['package']}" do
-  source "http://#{node['chef-server']['repo']['fqdn']}/#{node['chef-server']['push']['package']}"
-  checksum "#{node['chef-server']['push']['checksum']}"
+remote_file "#{node['chef-server']['install_path']}/#{node['chef-server']['report']['package']}" do
+  source "http://#{node['chef-server']['repo']['fqdn']}/#{node['chef-server']['report']['package']}"
+  checksum "#{node['chef-server']['report']['checksum']}"
 end
 
-execute "chef-server-ctl install opscode-push-jobs-server --path #{node['chef-server']['install_path']}"
+execute "chef-server-ctl install opscode-reporting --path #{node['chef-server']['install_path']}"
 
-execute "opscode-push-jobs-server-ctl reconfigure"
-
-remote_file "#{node['chef-server']['install_path']}/#{node['chef-server']['sync']['package']}" do
-  source "http://#{node['chef-server']['repo']['fqdn']}/#{node['chef-server']['sync']['package']}"
-  checksum "#{node['chef-server']['sync']['checksum']}"
-end
-
-execute "chef-server-ctl install chef-sync --path #{node['chef-server']['install_path']}"
-
-# execute "chef-sync-ctl reconfigure"
+execute "opscode-reporting-ctl reconfigure"
 
 execute "chef-server-ctl reconfigure"
+
+# remote_file "#{node['chef-server']['install_path']}/#{node['chef-server']['push']['package']}" do
+#   source "http://#{node['chef-server']['repo']['fqdn']}/#{node['chef-server']['push']['package']}"
+#   checksum "#{node['chef-server']['push']['checksum']}"
+# end
+#
+# execute "chef-server-ctl install opscode-push-jobs-server --path #{node['chef-server']['install_path']}"
+#
+# execute "opscode-push-jobs-server-ctl reconfigure"
+#
+# remote_file "#{node['chef-server']['install_path']}/#{node['chef-server']['sync']['package']}" do
+#   source "http://#{node['chef-server']['repo']['fqdn']}/#{node['chef-server']['sync']['package']}"
+#   checksum "#{node['chef-server']['sync']['checksum']}"
+# end
+#
+# execute "chef-server-ctl install chef-sync --path #{node['chef-server']['install_path']}"
+#
+# execute "chef-sync-ctl reconfigure"
