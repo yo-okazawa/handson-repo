a478e9eb4427e1ece4907738b2bd089acb2299e3
Hiroyuki Urasoko <ura.58.58@gmail.com>
2014-11-20 15:26:33 +0900

    install push-server

--- cookbooks/chef-server/recipes/post.rb
+++ cookbooks/chef-server/recipes/post.rb
@@ -1,3 +1,14 @@
+remote_file "#{node['chef-server']['install_path']}/#{node['chef-server']['push']['package']}" do
+  source "http://#{node['chef-server']['repo']['fqdn']}/#{node['chef-server']['push']['package']}"
+  checksum "#{node['chef-server']['push']['checksum']}"
+end
+
+execute "chef-server-ctl install opscode-push-jobs-server --path #{node['chef-server']['install_path']}"
+
+execute "opscode-push-jobs-server-ctl reconfigure"
+
+execute "chef-server-ctl reconfigure"
+
 remote_file "#{node['chef-server']['install_path']}/#{node['chef-server']['report']['package']}" do
   source "http://#{node['chef-server']['repo']['fqdn']}/#{node['chef-server']['report']['package']}"
   checksum "#{node['chef-server']['report']['checksum']}"
@@ -9,15 +20,6 @@ execute "opscode-reporting-ctl reconfigure"
 
 execute "chef-server-ctl reconfigure"
 
-# remote_file "#{node['chef-server']['install_path']}/#{node['chef-server']['push']['package']}" do
-#   source "http://#{node['chef-server']['repo']['fqdn']}/#{node['chef-server']['push']['package']}"
-#   checksum "#{node['chef-server']['push']['checksum']}"
-# end
-#
-# execute "chef-server-ctl install opscode-push-jobs-server --path #{node['chef-server']['install_path']}"
-#
-# execute "opscode-push-jobs-server-ctl reconfigure"
-#
 # remote_file "#{node['chef-server']['install_path']}/#{node['chef-server']['sync']['package']}" do
 #   source "http://#{node['chef-server']['repo']['fqdn']}/#{node['chef-server']['sync']['package']}"
 #   checksum "#{node['chef-server']['sync']['checksum']}"
