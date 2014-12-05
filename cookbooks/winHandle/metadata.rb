72e0741767c2f1abb697e3d6f70c5da1db9a5735
hiroki-arai <h-arai@creationline.com>
2014-12-05 20:31:35 +0900

    create winHandle #11

---
+++ cookbooks/winHandle/metadata.rb
@@ -0,0 +1,8 @@
+name             'winHandle'
+maintainer       'The Authors'
+maintainer_email 'you@example.com'
+license          'all_rights'
+description      'Installs/Configures winHandle'
+long_description 'Installs/Configures winHandle'
+version          '0.1.0'
+
---
+++ cookbooks/winHandle/recipes/default.rb
@@ -0,0 +1,15 @@
+#
+# Cookbook Name:: winHandle
+# Recipe:: default
+#
+
+
+#first test echo
+powershell_script "echo Helloworld" do
+
+end
+
+#service iis start
+service "IISADMIN" do
+  action [ :enable, :start ]
+end
