47c0d726f352a6959a211b38baff1d1c5fc5d704
Hiroyuki Urasoko <ura.58.58@gmail.com>
2014-12-09 15:53:20 +0900

    delete analytics attribute

--- cookbooks/chef-server/attributes/default.rb
+++ cookbooks/chef-server/attributes/default.rb
@@ -1,13 +1,11 @@
-default['chef-server']['anltcs']['fqdn'] = default['chef-server']['ws']['fqdn']
-
 default['chef-server']['topology'] = 'ha'
 
 default['chef-server']['core']['package'] = 'chef-server-core-12.0.0-1.el6.x86_64.rpm'
 default['chef-server']['core']['checksum'] = '89e212e7ed46b30c69a4dbfe568d9bfc299f8a6293f69e4d776f40c2d96aebeb'
 default['chef-server']['manage']['package'] = 'opscode-manage-1.6.2-1.el6.x86_64.rpm'
 default['chef-server']['manage']['checksum'] = '59100815a5e86f2a9de6beb03605d1b90e93f5ef0eecf336222f46a7e29f340b'
-default['chef-server']['push']['package'] = 'opscode-push-jobs-server-1.1.5-1.x86_64.rpm'
-default['chef-server']['push']['checksum'] = '4b8d5c825631932bc3432d3dc0a0a07f4d85f0e48af7671ef7d2bd1d37a68069'
+default['chef-server']['push']['package'] = 'opscode-push-jobs-server-1.1.6-1.x86_64.rpm'
+default['chef-server']['push']['checksum'] = '8377efdb563dc86d77968fdb42d9772a8b367f7083a399c8063975cbc1f393e1'
 default['chef-server']['sync']['package'] = 'chef-sync-1.0.0_rc.3-1.x86_64.rpm'
 default['chef-server']['sync']['checksum'] = '8c91c247ce07da02f56a05687dee69038deab4ba1637e8b9d10bbf83c1b0306d'
 default['chef-server']['report']['package'] = 'opscode-reporting-1.2.1-1.x86_64.rpm'
