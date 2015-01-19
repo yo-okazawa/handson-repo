61adae6e94d43a0e6ef4a8b92b446252b24f6893
Hiroyuki Urasoko <ura.58.58@gmail.com>
2015-01-19 13:26:22 +0900

    add hosts config

--- cookbooks/chef-server/recipes/anltcs.rb
+++ cookbooks/chef-server/recipes/anltcs.rb
@@ -1,7 +1,15 @@
 execute 'yum update -y'
 
+hostsfile_entry "#{node['chef-server']['api']['ipaddr']}" do
+  hostname  "#{node['chef-server']['api']['fqdn']}"
+end
+
+hostsfile_entry "#{node['chef-server']['rp_vip']['ipaddr']}" do
+  hostname  "#{node['chef-server']['rp_vip']['fqdn']}"
+end
+
 remote_file "#{node['chef-server']['install_path']}/#{node['chef-server']['anltcs']['package']}" do
-  source "http://#{node['chef-server']['repo']['url']}/#{node['chef-server']['anltcs']['package']}"
+  source "http://#{node['chef-server']['rp1']['fqdn']}/packages/#{node['chef-server']['anltcs']['package']}"
   checksum "#{node['chef-server']['anltcs']['checksum']}"
 end
 
