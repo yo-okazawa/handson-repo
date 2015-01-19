0bf4dc021de52ef434fce740f3211f4eac1e9dcd
Hiroyuki Urasoko <ura.58.58@gmail.com>
2015-01-19 13:29:23 +0900

    add self certified

--- cookbooks/repo-server/recipes/default.rb
+++ cookbooks/repo-server/recipes/default.rb
@@ -4,7 +4,19 @@
 #
 # Copyright (c) 2014 The Authors, All Rights Reserved.
 rpm_package "nginx" do
-  source "/tmp/nginx-1.7.8-1.el6.ngx.x86_64.rpm"
+  source "/tmp/nginx-1.7.9-1.el6.ngx.x86_64.rpm"
+end
+
+cookbook_file "/etc/nginx/chefrepo.cloud-platform.kddi.ne.jp.crt" do
+  source "chefrepo.cloud-platform.kddi.ne.jp.crt"
+end
+
+cookbook_file "/etc/nginx/chefrepo.cloud-platform.kddi.ne.jp.key" do
+  source "chefrepo.cloud-platform.kddi.ne.jp.key"
+end
+
+cookbook_file "/etc/nginx/conf.d/example_ssl.conf" do
+  source "example_ssl.conf"
 end
 
 file "/usr/share/nginx/html/check.html" do
@@ -13,5 +25,5 @@ end
 
 service 'nginx' do
   supports :status => true, :restart => true, :reload => true
-  action [:enable, :start]
+  action [:enable, :start, :reload]
 end
