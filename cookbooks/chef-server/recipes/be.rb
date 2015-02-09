e0817c4068b622da327366b016f3779feb1869f2
Hiroyuki Urasoko <h-urasoko@creationline.com>
2015-02-09 15:17:30 +0900

    push job server must  install to back end only

--- cookbooks/chef-server/recipes/be.rb
+++ cookbooks/chef-server/recipes/be.rb
@@ -2,4 +2,15 @@ include_recipe "chef-server::drbd"
 
 execute "chef-server-ctl reconfigure"
 
+remote_file "#{node['chef-server']['install_path']}/#{node['chef-server']['push']['package']}" do
+  source "http://#{node['chef-server']['rp1']['fqdn']}/packages/#{node['chef-server']['push']['package']}"
+  checksum "#{node['chef-server']['push']['checksum']}"
+end
+
+execute "chef-server-ctl install opscode-push-jobs-server --path #{node['chef-server']['install_path']}"
+
+execute "opscode-push-jobs-server-ctl reconfigure"
+
+execute "chef-server-ctl reconfigure"
+
 include_recipe "chef-server::post"
