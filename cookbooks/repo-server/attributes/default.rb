bcb3d99b36d29139e0b50df79485b718eaad68f7
hiroki-arai <h-arai@creationline.com>
2015-03-06 11:23:05 +0900

    upgrade repo-server

--- cookbooks/repo-server/attributes/default.rb
+++ cookbooks/repo-server/attributes/default.rb
@@ -6,10 +6,21 @@
 default["repo-server"]["rpm-package"] = "/tmp/nginx-1.7.10-1.el6.ngx.x86_64.rpm"
 
 default["repo-server"]["shell"] = "/usr/local/bin/rsync-centos-yum-mirror.sh"
-default["repo-server"]["packages"] = "/usr/share/nginx/html/packages"
 default["repo-server"]["source"] = "rsync://rsync.kddilabs.jp/centos/"
 default["repo-server"]["dastination"] = "/usr/share/nginx/html/packages/centos/"
-default["repo-server"]["log"] = "/var/log/rsync-centos-yum-mirror.log"
+default["repo-server"]["log"] = "/var/log/rsync/rsync-centos-yum-mirror.log"
+
+default["repo-server"]["directories"] = [
+  "/usr/share/nginx/html/packages",
+  "/usr/share/nginx/html/packages/chef",
+  "/usr/share/nginx/html/packages/chef/bootstrap",
+  "/usr/share/nginx/html/packages/chef/gems",
+  "/usr/share/nginx/html/packages/chef/packages",
+  "/usr/share/nginx/html/packages/mackerel",
+  "#{default["repo-server"]["dastination"]}",
+  "/usr/share/nginx/html/packages/oracle",
+  "/var/log/rsync"
+]
 
 if "#{node["hostname"][-1]}" == "1"
   default["repo-server"]["rsync"]["first"] = true
--- cookbooks/repo-server/recipes/default.rb
+++ cookbooks/repo-server/recipes/default.rb
@@ -4,51 +4,100 @@
 #
 # Copyright (c) 2014 The Authors, All Rights Reserved.
 
+#
+#nginx install and configuration
+#
+
 #install nginx from local package
 rpm_package "nginx" do
   source "#{node["repo-server"]["rpm-package"]}"
 end
 
+#service nginx setting
+service 'nginx' do
+  supports :status => true, :restart => true, :reload => true
+end
+
+#put nginx.conf
+cookbook_file "/etc/nginx/nginx.conf" do
+  source "nginx.conf"
+  owner 'root'
+  group 'root'
+  mode '0644'
+  notifies :reload, 'service[nginx]', :delayed
+end
+
+#put repo-server.conf
+cookbook_file "/etc/nginx/conf.d/repo-server.conf" do
+  source "repo-server.conf"
+  owner 'root'
+  group 'root'
+  mode '0644'
+  notifies :reload, 'service[nginx]', :delayed
+end
+
+bash "rsync mirror" do
+  user "root"
+  cwd "/etc/nginx/conf.d/"
+  code "mv default.conf default.conf.org"
+  only_if { File.exists?("/etc/nginx/conf.d/default.conf") }
+  notifies :reload, 'service[nginx]', :delayed
+end
+
+bash "mv example_ssl.conf" do
+  user "root"
+  cwd "/etc/nginx/conf.d/"
+  code "mv example_ssl.conf example_ssl.conf.org"
+  only_if { File.exists?("/etc/nginx/conf.d/example_ssl.conf") }
+  notifies :reload, 'service[nginx]', :delayed
+end
+
+#
+#make directories
+#
+
+#make directories
+node["repo-server"]["directories"].each do |dir|
+  directory dir do
+    action :create
+  end
+end
+
+
+#
+#put files
+#
+
+#put .htapasswd
+cookbook_file "/etc/nginx/.htpasswd" do
+  source ".htpasswd"
+  owner 'nginx'
+  group 'root'
+  mode '0600'
+  sensitive true
+end
+
 #put crt file
 cookbook_file "/etc/nginx/chefrepo.cloud-platform.kddi.ne.jp.crt" do
   source "chefrepo.cloud-platform.kddi.ne.jp.crt"
+  owner 'root'
+  group 'root'
+  mode '0644'
   sensitive true
 end
 
 #put key file
 cookbook_file "/etc/nginx/chefrepo.cloud-platform.kddi.ne.jp.key" do
   source "chefrepo.cloud-platform.kddi.ne.jp.key"
-  sensitive true
-end
-
-#put nginx conf
-cookbook_file "/etc/nginx/conf.d/example_ssl.conf" do
-  source "example_ssl.conf"
+  owner 'root'
+  group 'root'
+  mode '0600'
   sensitive true
 end
 
 #make check.html file
 file "/usr/share/nginx/html/check.html" do
   content "html-check OK"
-end
-
-#service nginx setting
-service 'nginx' do
-  supports :status => true, :restart => true, :reload => true
-  action [:enable, :start, :reload]
-end
-
-#make packages dir
-directory "#{node["repo-server"]["packages"]}" do
-  owner 'root'
-  group 'root'
-  mode '0755'
-  action :create
-end
-
-#make mirror dir
-directory "#{node["repo-server"]["dastination"]}" do
-  action :create
 end
 
 template "#{node["repo-server"]["shell"]}" do
@@ -60,12 +109,26 @@ template "#{node["repo-server"]["shell"]}" do
   action :create
 end
 
-#rcync mirror
-bash "rsync mirror" do
-  user "root"
-  code "sh #{node["repo-server"]["shell"]} >> #{node["repo-server"]["log"]} &"
-  only_if {node["repo-server"]["rsync"]["first"]}
+#put logrotate nginx file
+cookbook_file "/etc/logrotate.d/nginx" do
+  source "nginx"
+  owner 'root'
+  group 'root'
+  mode '0644'
 end
+
+#put logrotate rcync file
+cookbook_file "/etc/logrotate.d/rsync" do
+  source "rsync"
+  owner 'root'
+  group 'root'
+  mode '0644'
+end
+
+
+#
+#rsync yum mirror configure and do
+#
 
 #add crontab
 ruby_block "add_crontab" do
@@ -80,3 +143,11 @@ ruby_block "add_crontab" do
     end
   end
 end
+
+#rcync mirror
+bash "rsync mirror" do
+  user "root"
+  code "sh #{node["repo-server"]["shell"]} >> #{node["repo-server"]["log"]} &"
+  only_if {node["repo-server"]["rsync"]["first"]}
+end
+
