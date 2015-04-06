723f5f6874166de3a8e01d4a9c8b7fc5e089e75c
hiroki-arai <h-arai@creationline.com>
2015-04-06 11:35:55 +0900

    modify gem version

--- cookbooks/knife-windows/attributes/default.rb
+++ cookbooks/knife-windows/attributes/default.rb
@@ -11,22 +11,22 @@ default["knife-windows"]["package"] = [
 ]
 
 default["knife-windows"]["gem-list"] = [
-  "akami-1.2.2.gem",
+  "akami-1.3.0.gem",
   "builder-3.2.2.gem",
   "em-winrm-0.6.0.gem",
-  "eventmachine-1.0.3.gem",
-  "ffi-1.9.6.gem",
+  "eventmachine-1.0.7.gem",
+  "ffi-1.9.8.gem",
   "gssapi-1.0.3.gem",
-  "gyoku-1.2.2.gem",
-  "httpclient-2.5.3.3.gem",
+  "gyoku-1.3.0.gem",
+  "httpclient-2.6.0.1.gem",
   "httpi-0.9.7.gem",
-  "knife-windows-0.8.2.gem",
+  "knife-windows-0.8.4.gem",
   "little-plugger-1.1.3.gem",
   "logging-1.8.2.gem",
-  "mini_portile-0.6.1.gem",
+  "mini_portile-0.6.2.gem",
   "mixlib-log-1.6.0.gem",
-  "multi_json-1.10.1.gem",
-  "nokogiri-1.6.5.gem",
+  "multi_json-1.11.0.gem",
+  "nokogiri-1.6.6.2.gem",
   "nori-1.1.5.gem",
   "rack-1.6.0.gem",
   "rubyntlm-0.1.1.gem",
@@ -34,7 +34,7 @@ default["knife-windows"]["gem-list"] = [
   "uuidtools-2.1.5.gem",
   "wasabi-1.0.0.gem",
   "winrm-1.2.0.gem",
-  "winrm-s-0.2.2.gem"
+  "winrm-s-0.2.4.gem"
 ]
 
 default["knife-windows"]["repo-url"] = "https://chefrepo.cloud-platform.kddi.ne.jp/packages/chef"
